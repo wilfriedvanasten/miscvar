ecc8a4615f082fe04e63016555dea16696055b6e
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-05 21:35:51 +0100

    Rewrite cpuuutilblock as a fish function

---
+++ functions/cpuutilblock.fish
@@ -0,0 +1,43 @@
+# A function that outputs a cpu load bar compatible with the tmux status bar
+# Supports low to full load and above.
+
+function _current_load
+	command cat /proc/loadavg | sed 's/^\([0-9][0-9]*\.[0-9][0-9]*\)[^0-9].*$/\1/g'
+end
+
+function cpuutilblock
+  set -l current_load (_current_load)
+	set -l num_cpus (command nproc)
+	set -l ld (math (math -s2 "$current_load / $num_cpus * 10 + 0.5") / 1)
+	set -l low "."
+	set -l high "|"
+	set -l color "green"
+	if [ $ld -gt 3 ]
+		set color "yellow"
+		if [ $ld -gt 8 ]
+			set color "red"
+		end
+	end
+	while [ $ld -gt 10 ]
+		set ld (math $ld - 10)
+		set low $high
+		if [ $high = "|" ]
+			set high 1
+		else
+			set high (math $high + 1)
+		end
+	end
+	echo -n "#[fg=$color]["
+	set -l c 0
+	while [ $c -lt $ld ]
+		echo -n $high
+		set c (math $c + 1)
+	end
+	set -l c 0
+	set -l l (math 10 - $ld)
+	while [ $c -lt $l ]
+		echo -n $low
+		set c (math $c + 1)
+	end
+	echo -n ']#[fg=default]'
+end
