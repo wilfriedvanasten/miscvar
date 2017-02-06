fb98f61b218a7af80439848537653cfb2e9279b1
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-06 22:00:33 +0100

    Use count to check if enough arguments are given
    The old method gave errors about expanding arguments

--- functions/shorten_path.fish
+++ functions/shorten_path.fish
@@ -1,5 +1,5 @@
 function shorten_path
-	if test -z $argv[2]
+	if test (count $argv) -lt 3
 		echo $argv[1] | sed -e "s#$HOME#~#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
 	else
 		echo $argv[1] | sed -e "s#$argv[2]#$argv[3]#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
