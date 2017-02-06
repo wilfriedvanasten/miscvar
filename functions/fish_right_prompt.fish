57712516352972721c30e8fbb1780d73bb4751dd
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-06 19:54:38 +0100

    Show virtual env in right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,3 +1,8 @@
 function fish_right_prompt
-
+  if set -q VIRTUAL_ENV
+    # Let's hope python won't break this way to read the version
+    set -l python_version (python --version | sed -e 's/Python //g')
+    set -l virtual_env_short (shorten_path $VIRTUAL_ENV)
+    echo "$virtual_env_short@$python_version"
+  end
 end
