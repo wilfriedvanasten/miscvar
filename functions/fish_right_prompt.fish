49cc038aa90d339ef31e19700ea7252eb36c2872
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-06 20:50:06 +0100

    Add color to virtual env

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -3,6 +3,9 @@ function fish_right_prompt
     # Let's hope python won't break this way to read the version
     set -l python_version (python --version | sed -e 's/Python //g')
     set -l virtual_env_short (shorten_path $VIRTUAL_ENV)
+    set_color -b yellow
+    set_color black
     echo "$virtual_env_short@$python_version"
+    set_color normal
   end
 end
