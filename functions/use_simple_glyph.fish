ec429390d62a2c86d4e335d2fa3b0774ca5a2672
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-07 10:39:30 +0100

    Add a FIXME to use_simple_glyph

--- functions/use_simple_glyph.fish
+++ functions/use_simple_glyph.fish
@@ -1,3 +1,8 @@
+# FIXME: This doesn't play nice with tmux/screen
+# I looked into it, but no solution seems readily
+# available. Going into a vt is a rare occurence
+# and I do not need tmux there, but it would be
+# nice to have.
 function use_simple_glyph --description 'Checks if simple glyphs should be used'
 	test $TERM = "linux"
 end
