c37c80719022fdc3b8e4a3b84e24249de6c20eec
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-13 16:55:34 +0100

    Only show host for ssh

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -10,6 +10,13 @@ function fish_title
       else
         set title "$_ *"
       end
+    case ssh
+      set -l args (string split " " $argv)
+      if test (count $args -eq 2)
+        set title (echo $args[2] | cut -d '.' -f 1)
+      else
+        set title "$argv"
+      end
     case '*'
     set title "$argv"
   end
