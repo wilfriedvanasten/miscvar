6872b2e681e9dfdf4d5c6e305221a0288301c4ca
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-13 14:31:23 +0100

    If in fish only show dir. For vim show file

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,4 +1,17 @@
 function fish_title
-	set -l title "$_ "(shorten_path (pwd))
-  printf "\033k$title\033\\"
+  set -l title ""
+  switch $_
+    case fish
+      set title (prompt_pwd)
+    case vim
+      set -l args (string split " " $argv)
+      if test (count $args -gt 1)
+        set title "$_ "(shorten_path $args[2])
+      else
+        set title "$_ *"
+      end
+    case '*'
+	    set title "$argv"
+ end
+ printf "\033k$title\033\\"
 end
