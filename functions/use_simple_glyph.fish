50c0a69a935251872bbfe87437207735e64ef1ab
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-02-20 10:41:19 +0100

    Allow manual toggle of simple glyphs

--- functions/use_simple_glyph.fish
+++ functions/use_simple_glyph.fish
@@ -4,5 +4,6 @@
 # and I do not need tmux there, but it would be
 # nice to have.
 function use_simple_glyph --description 'Checks if simple glyphs should be used'
-	test $TERM = "linux"
+  set -q config_use_simple_glyph
+	  or test $TERM = "linux"
 end
