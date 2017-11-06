a200f1162a1e698053ad635a2577345eab2134dd
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-11-06 16:00:24 +0100

    Use simpler aligment construct

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -172,10 +172,15 @@ function _prompt_git
         set git_glyphs "$git_glyphs $tag_glyph"
         set git_status_color red
     end
-    _prompt_fletching
-    _prompt_segment $git_status_color $git_context_line
-    _prompt_arrow
-    echo
+    echo -n "  "
+    set_color $git_status_color
+    if use_simple_glyph
+      echo -n "|"
+    else
+      echo -n "│"
+    end
+    echo $git_context_line
+    set_color $prompt_color
     _prompt_fletching
     if test $git_status_symbols
       _prompt_segment $git_status_color "$git_project_path $git_status_symbols"
