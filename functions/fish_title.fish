37cf2ebce455f3a2b10d75fa1129ef753d02de09
Wilfried van Asten <wilfriedvanasten@gmail.com>
2017-12-08 14:20:06 +0100

    Use the commandline buffer for tokenization
    The original approaches would have broken up arguments with spaces. This
    should prevent that.

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -2,20 +2,26 @@ function _ssh_find_config_value
  ssh -G $argv[2..-1] | grep -ie "^$argv[1] " | sed "s/^$argv[1] \(.*\)/\1/I"
 end
 
+function _tokenize_command
+  commandline -r $argv[1]
+  commandline -o
+  commandline -r ""
+end
+
 function fish_title
   set -l title ""
   switch $_
     case fish
       set title (prompt_pwd)
     case vim
-      set -l args (echo $argv[1] | sed 's/ /\n/g')
+      set -l args (_tokenize_command $argv[1])
       if test (count $args) -gt 1
         set title "$_ "(shorten_path $args[2])
       else
         set title "$_ *"
       end
     case ssh
-      set -l args (echo $argv[1] | sed 's/ /\n/g')
+      set -l args (_tokenize_command $argv[1])
       set ssh_user (_ssh_find_config_value "User" $args[2..-1])
       set ssh_host (_ssh_find_config_value "Hostname" $args[2..-1] | cut -d '.' -f 1)
       if test (count $args -eq 2)
