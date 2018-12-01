21370688b07bde9efab45175aef2614217175e97
Wilfried van Asten <wilfriedvanasten@gmail.com>
2018-12-01 15:16:31 +0100

    Add local bins for cabal installed user binaries

--- config.fish
+++ config.fish
@@ -4,5 +4,7 @@ set -xg SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 # Use vim for git
 set -xg VISUAL "vim"
 
+set -xg PATH ~/.local/bin $PATH
+
 # T(he )tmux session
 alias ttmux "tmux attach-session; or tmux"
