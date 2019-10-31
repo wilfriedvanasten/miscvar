5fff561cd43d3d1a09de5adbb39a6a4b9fafbcf4
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-10-31 17:22:10 +0100

    Fix shorten path incorrectly shortening files in /home

--- functions/shorten_path.fish
+++ functions/shorten_path.fish
@@ -1,7 +1,7 @@
 function shorten_path
   if test (count $argv) -lt 3
-    echo $argv[1] | sed -e "s#$HOME#~#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
+    echo $argv[1] | sed -e "s#$HOME\\(/\\|\$\\)#~\1#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
   else
-    echo $argv[1] | sed -e "s#$argv[2]#$argv[3]#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
+    echo $argv[1] | sed -e "s#$argv[2]\\(/\\|\$\\)#$argv[3]\1#g" | sed -e 's#\\(\\.\\?[^/]\\{1\\}\\)[^/]*/#\\1/#g'
   end
 end
