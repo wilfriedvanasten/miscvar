95e9aefc1c56422b6bad94651d6bad52bce650e4
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-01 13:28:47 +0100

    Replace usages of expr with string

--- functions/fold_string.fish
+++ functions/fold_string.fish
@@ -1,12 +1,12 @@
 function fold_string
   set -l fold_replace $argv[1]
   set -l fold_length $argv[2]
-  if [ (expr length + $argv[3]) -le $fold_length ]
+  if [ (string length "$argv[3]") -le $fold_length ]
     # If the string fits in the fold length no folding is necessary
     echo $argv[3]
   else
-    set -l prefix_length (math -s 0 $fold_length - (expr length + $fold_replace))
-    set -l prefix (expr substr $argv[3] 1 $prefix_length)
+    set -l prefix_length (math -s 0 $fold_length - (string length "$fold_replace"))
+    set -l prefix (string sub -s 1 -l $prefix_length $argv[3])
     echo -n $prefix
     echo $fold_replace
   end
