fb1b02f72b7ec65a68cb4b106c9adbcfef06ed5f
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-02 00:06:10 +0100

    Move all fish stuff in a fish directory
    Symlinks are created in the top-level to keep
    existing links into clones working

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+# Make sure ssh-agent can be reached
+set -xg SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# Use vim for git
+set -xg VISUAL "vim"
+
+set -xg PATH ~/.local/bin $PATH
+
+# T(he )tmux session
+alias ttmux "tmux attach-session; or tmux"
