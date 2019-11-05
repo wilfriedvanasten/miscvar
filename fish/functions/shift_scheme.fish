5a4009a3f894b0d057150a9f2ebfdb8a8a49057a
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-05 19:34:57 +0100

    Implement a function to set a Shift-like color scheme

---
+++ fish/functions/shift_scheme.fish
@@ -0,0 +1,28 @@
+function shift_scheme
+  set fish_color_autosuggestion 3a3a3a brblack
+  set fish_color_cancel -r
+  set fish_color_command ffbf00
+  set fish_color_comment 7f7f7f
+  # These colors are not used in the prompt
+  set fish_color_cwd normal
+  set fish_color_cwd_root normal
+  set fish_color_end --bold ff7f00
+  set fish_color_error ff0000
+  set fish_color_escape 00bf00
+  set fish_color_history_current normal
+  # This color is not used
+  set fish_color_host normal
+  # TODO: Setting this didn't seem to have any effect?
+  set fish_color_match normal
+  set fish_color_normal normal
+  set fish_color_operator d5d500
+  set fish_color_param bfff00
+  set fish_color_quote --bold ffffff
+  set fish_color_redirection d5d500
+  # TODO: What does this do again
+  set fish_color_search_match ffff00
+  set fish_color_selection c0c0c0
+  set fish_color_status red
+  set fish_color_user normal
+  set fish_color_valid_path --underline
+end
