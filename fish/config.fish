c5504cb7e78557a275147eae33242ad42cfc7cf7
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-06 17:24:55 +0100

    Only run tmux related code in interactive shell

--- fish/config.fish
+++ fish/config.fish
@@ -8,12 +8,14 @@ set -xg PATH ~/.local/bin $PATH
 
 # T(he )tmux session
 
-if not tmux has -t "default" 2> /dev/null
-  tmux new -d -s "default"
-end
-if test -z "$TMUX"
-  set default_is_attached (string split ' ' (tmux ls -F '#{session_name} #{session_attached}' 2> /dev/null | grep "default"))[2]
-  if test "$default_is_attached" = "0"
-    exec tmux attach -t "default"
+if status --is-interactive
+  if not tmux has -t "default" 2> /dev/null
+    tmux new -d -s "default"
+  end
+  if test -z "$TMUX"
+    set default_is_attached (string split ' ' (tmux ls -F '#{session_name} #{session_attached}' 2> /dev/null | grep "default"))[2]
+    if test "$default_is_attached" = "0"
+      exec tmux attach -t "default"
+    end
   end
 end
