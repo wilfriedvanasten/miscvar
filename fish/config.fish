6e8fe3c93642a5b46d38c5b27c4d16c8ece49d03
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-06 17:14:12 +0100

    Improve silencing of commands

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ set -xg PATH ~/.local/bin $PATH
 
 # T(he )tmux session
 
-if not tmux has -t "default" 2>&1 > /dev/null
+if not tmux has -t "default" 2> /dev/null
   tmux new -d -s "default"
 end
 if test -z "$TMUX"
