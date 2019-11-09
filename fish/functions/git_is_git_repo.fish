893c45b116af7b8574f82505a94be7ecb66de9f8
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-09 04:07:32 +0100

    Make git_is_git_repo cheaper

--- fish/functions/git_is_git_repo.fish
+++ fish/functions/git_is_git_repo.fish
@@ -1,5 +1,5 @@
 # A function to check if the current directory
 # is or is part of a git repo
 function git_is_git_repo
-  command git status 2> /dev/null > /dev/null
+  command git rev-parse --is-inside-work-tree 2> /dev/null > /dev/null
 end
