253e4c52d2536008c5079b088d852205289cab90
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-09 04:28:04 +0100

    Make git_exists much more efficient

--- fish/functions/git_exists.fish
+++ fish/functions/git_exists.fish
@@ -1,3 +1,5 @@
 function git_exists -d 'Checks if git is present'
-  type git 2> /dev/null >/dev/null
+  # Use which instead of type. We always use command to execute
+  # git and this is much, much cheaper
+  which git 2> /dev/null >/dev/null
 end
