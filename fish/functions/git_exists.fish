a19f50df0b386d8a54ee0269b6dfa310abef4d01
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-09 05:25:35 +0100

    Use command -s instead of which

--- fish/functions/git_exists.fish
+++ fish/functions/git_exists.fish
@@ -1,5 +1,5 @@
 function git_exists -d 'Checks if git is present'
-  # Use which instead of type. We always use command to execute
-  # git and this is much, much cheaper
-  which git 2> /dev/null >/dev/null
+  # Use command -s instead of type. which would also
+  # work, but a builtin must work
+  command -s git 2> /dev/null >/dev/null
 end
