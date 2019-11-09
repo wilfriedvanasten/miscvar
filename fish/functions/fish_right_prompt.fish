f8f88d37a55313da4cc788961b8c1aa00459b38a
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-09 03:54:16 +0100

    Implement a utility and a prompt to deal with symlink pwds

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,4 +1,12 @@
 function fish_right_prompt
+  if test (pwd -L) != (pwd -P)
+    if git_exists
+      and git_is_git_repo
+      echo -n "<"(shorten_path (pwd -P) (command git rev-parse --show-toplevel) ":")">"
+    else
+      echo -n "<"(shorten_path (pwd -P))">"
+    end
+  end
   if set -q VIRTUAL_ENV
     # Let's hope python won't break this way to read the version
     set -l python_version (python --version | sed -e 's/Python //g')
