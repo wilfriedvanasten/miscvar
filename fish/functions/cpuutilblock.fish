4a72bcde51786bf72f6c47b5130d79b182e84c53
Wilfried van Asten <wilfriedvanasten@gmail.com>
2019-11-28 10:56:03 +0100

    Fix block size for full load

--- fish/functions/cpuutilblock.fish
+++ fish/functions/cpuutilblock.fish
@@ -18,7 +18,7 @@ function cpuutilblock
   end
   set -l blocks '▁' '▂' '▃' '▄' '▅' '▆' '▇' '█'
   echo -n "#[fg=$color]"
-  set -l block 7
+  set -l block 8
   if test "$perc" -lt 100
     set block (math (math -s0 "(($ld / (100 / 7) + 0.5)/1)") + 1)
   end
